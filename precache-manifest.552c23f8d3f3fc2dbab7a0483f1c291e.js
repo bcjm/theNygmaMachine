92b6ac9e3beeb8b62cc4d121377d2121f7e13968
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-03-07 17:03:29 -0500

    Updates

---
+++ precache-manifest.552c23f8d3f3fc2dbab7a0483f1c291e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23426e73d0aa5526de1acebf3c00f8e3",
+    "url": "/theNygmaMachine/index.html"
+  },
+  {
+    "revision": "f6c1113571c89b0e12ab",
+    "url": "/theNygmaMachine/static/css/main.5762a348.chunk.css"
+  },
+  {
+    "revision": "26a2995af61470f5ac61",
+    "url": "/theNygmaMachine/static/js/2.13516475.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/theNygmaMachine/static/js/2.13516475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c1113571c89b0e12ab",
+    "url": "/theNygmaMachine/static/js/main.99dd47b6.chunk.js"
+  },
+  {
+    "revision": "082b202da86733db2340",
+    "url": "/theNygmaMachine/static/js/runtime-main.ebb190c8.js"
+  }
+]);
