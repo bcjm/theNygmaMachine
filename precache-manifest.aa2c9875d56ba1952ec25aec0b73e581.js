bd3ef5059516c8afb211ddab69d44feb339ddf54
Brittany Giles <giles.brittany@gmail.com>
2020-03-11 10:11:26 -0400

    Updates

---
+++ precache-manifest.aa2c9875d56ba1952ec25aec0b73e581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388622e5838852f56b39e38004bd409d",
+    "url": "/theNygmaMachine/index.html"
+  },
+  {
+    "revision": "4426104b27befb729974",
+    "url": "/theNygmaMachine/static/css/main.b9ee0b3d.chunk.css"
+  },
+  {
+    "revision": "33ec5bb6a3d993cdfe6f",
+    "url": "/theNygmaMachine/static/js/2.00e6ada8.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/theNygmaMachine/static/js/2.00e6ada8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4426104b27befb729974",
+    "url": "/theNygmaMachine/static/js/main.36e10c94.chunk.js"
+  },
+  {
+    "revision": "082b202da86733db2340",
+    "url": "/theNygmaMachine/static/js/runtime-main.ebb190c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/theNygmaMachine/precache-manifest.fe8db4d297754f58579b18cfb4c7a801.js"
+  "/theNygmaMachine/precache-manifest.aa2c9875d56ba1952ec25aec0b73e581.js"
 );
 
 self.addEventListener('message', (event) => {
